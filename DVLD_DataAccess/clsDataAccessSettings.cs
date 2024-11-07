6d13f300d524a3523e6a48c06ca1eeb763563cb6
ashraf <ashraf59121719@gmail.com>
2024-11-08 00:37:05 +0200

    This is a license management project

---
+++ DVLD_DataAccess/clsDataAccessSettings.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace DVLD_DataAccess
+{
+    static class clsDataAccessSettings
+    {
+        public static string ConnectionString = @"server=DESKTOP-I3NIFAB;database = DVLD;Encrypt=false;Integrated Security = true ";
+
+
+    }
+}
